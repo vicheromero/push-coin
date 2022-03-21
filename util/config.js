42a488d04c92f34879f338b9b13ba2cf1cfaba0b
Sebastian <odm_sd@hotmail.com>
2022-03-21 10:30:27 -0500

    new: change serialize new line

--- util/config.js
+++ util/config.js
@@ -15,7 +15,7 @@ const spinner = load({
 function getKey(path, key) {
     let value = undefined;
     try {
-        const data = fs.readFileSync(path, 'utf8').split('\r').map(i => i.replace("\n", '')).filter(i => i !== '');
+        const data = fs.readFileSync(path, 'utf8').replace(/\r\n/g,'\n').split('\n').filter(i=>i!=='');
         const valueArr = data.filter(j => j.includes(key));
         if (valueArr.length === 1) {
             value = valueArr[0].split(':');
