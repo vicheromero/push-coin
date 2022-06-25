072c7cdc8a615adc55e48b9a3352bba6efaa7801
SebastiaN <sebastian.robalino@inpsercom.com>
2022-06-25 16:47:34 -0500

    new: comando estatus completo

--- bin/cmds/status.js
+++ bin/cmds/status.js
@@ -1,3 +1,6 @@
+const constantes = require("../../util/const");
+const {spinner, printError} = require("../../util/config");
+const lng = require("../../util/en");
 exports.command = 'status'
 exports.builder = {
     path: {
@@ -6,5 +9,16 @@ exports.builder = {
 }
 exports.desc = 'Check status of push service'
 exports.handler = function () {
-    console.log('Status')
+    console.log('Status');
+    exec('systemctl status ' + constantes.appName, (error, stdout, stderr) => {
+        if (error) {
+            spinner.fail(printError(error));
+            return;
+        }
+        if (stderr) {
+            spinner.fail(printError(stderr));
+            return;
+        }
+        console.log(stdout);
+    })
 }
