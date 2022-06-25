c29c77fabc57b96e4356b225247b4f001ac5de37
SebastiaN <sebastian.robalino@inpsercom.com>
2022-06-25 16:47:09 -0500

    new: comando desinstalar completo

--- bin/cmds/uninstall.js
+++ bin/cmds/uninstall.js
@@ -1,5 +1,14 @@
+const constantes = require("../../util/const");
+const {printInfo, spinner} = require("../../util/config");
+const lng = require("../../util/en");
 exports.command = 'uninstall'
 exports.desc = 'Uninstall push service'
 exports.handler = function () {
-    console.log('uninstall')
+    console.log('uninstall');
+    spinner.info(printInfo("Desinstalando"));
+    spinner.start();
+    exec('sudo systemctl '+ constantes.appName, (error, stdout, stderr) => {
+
+    });
+
 }
